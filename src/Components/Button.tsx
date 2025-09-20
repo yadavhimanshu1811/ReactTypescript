e6bc781d786aad673222eba28679abd32fa0d402
yadavhimanshu1811 <yadavhimanshu1811@gmail.com>
2025-09-20 16:17:32 +0530

    installed bootstrap and made button and message components

---
+++ src/Components/Button.tsx
@@ -0,0 +1,9 @@
+function Button(){
+    const buttonText = "Click me";
+    return <button
+        color="red"
+        
+    >{buttonText}</button>;
+}
+
+export default Button;
---
+++ src/Components/Message.tsx
@@ -0,0 +1,10 @@
+function Message() { // Function based component;
+    const name = "Himanshu";
+    if(1){
+        return (
+            <h1>Hello {name}</h1>
+        )
+    }else return null;
+}
+
+export default Message;
