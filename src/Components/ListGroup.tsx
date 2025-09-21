116d34410471db298736dc4619009c0c5970ec96
yadavhimanshu1811 <yadavhimanshu1811@gmail.com>
2025-09-21 12:46:33 +0530

    Handling events

--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -1,24 +1,47 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   let items = ["Delhi", "Mumbai", "Jaipur"];
-  items = [];
+  //   items = [];
 
-  const getErrorMessage = () =>{
+  const getErrorMessage = () => {
     return items.length == 0 ? <p>Add some items</p> : null;
-  }
+  };
+  const handleClick = (event: MouseEvent) => console.log(event)
 
   return (
     <>
       {items.length ? (
-      <ul className="list-group">
-        <li className="list-group-item">Cras justo odio</li>
-        <li className="list-group-item">Dapibus ac facilisis in</li>
-        {items.map((it) => (
-          <li key={it} className="list-group-item">
-            {it}
+        <ul className="list-group">
+          <li
+            className="list-group-item"
+            onClick={(event) => {
+              console.log(event);
+            }}
+          >
+            Cras justo odio
           </li>
-        ))}
-      </ul>
-      ) : <div>No items found</div>}
+          <li
+            className="list-group-item"
+            onClick={handleClick}   //dont call the function
+          >
+            Dapibus ac facilisis in
+          </li>
+          {items.map((it, index) => (
+            <li
+              key={it}
+              className="list-group-item"
+              onClick={(event) => {
+                console.log(it, index, event);
+              }}
+            >
+              {it}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div>No items found</div>
+      )}
       {getErrorMessage()}
     </>
   );
