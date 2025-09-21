20b58dbc7abd4a070145873c26495bb4a92993ba
yadavhimanshu1811 <yadavhimanshu1811@gmail.com>
2025-09-21 12:32:26 +0530

    Using functions

--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -1,16 +1,26 @@
 function ListGroup() {
-  const items = ["Delhi", "Mumbai", "Jaipur"];
+  let items = ["Delhi", "Mumbai", "Jaipur"];
+  items = [];
+
+  const getErrorMessage = () =>{
+    return items.length == 0 ? <p>Add some items</p> : null;
+  }
 
   return (
-    <ul className="list-group">
-      <li className="list-group-item">Cras justo odio</li>
-      <li className="list-group-item">Dapibus ac facilisis in</li>
-      {items.map((it) => (
-        <li key={it} className="list-group-item">
-          {it}
-        </li>
-      ))}
-    </ul>
+    <>
+      {items.length ? (
+      <ul className="list-group">
+        <li className="list-group-item">Cras justo odio</li>
+        <li className="list-group-item">Dapibus ac facilisis in</li>
+        {items.map((it) => (
+          <li key={it} className="list-group-item">
+            {it}
+          </li>
+        ))}
+      </ul>
+      ) : <div>No items found</div>}
+      {getErrorMessage()}
+    </>
   );
 }
 
