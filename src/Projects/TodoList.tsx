11e4f38f2cd382b2e51bd07264ffcc1d7a722d40
yadavhimanshu1811 <yadavhimanshu1811@gmail.com>
2025-10-04 20:31:39 +0530

    todo list done except CSS

--- src/Projects/TodoList.tsx
+++ src/Projects/TodoList.tsx
@@ -4,6 +4,7 @@ import Button from "../Components/Button";
 function TodoList() {
   const [list, setList] = useState<string[]>([]);
   const [inputValue, setInputValue] = useState("");
+  const [taskDoneIndexes, setTaskDoneIndexes] = useState<number[]>([]);
 
   const addToList = () => {
     if (inputValue.length == 0) {
@@ -17,6 +18,15 @@ function TodoList() {
   const deleteFromList = (index: number) => {
     list.splice(index, 1);
     setList([...list]);
+  };
+
+  const taskDone = (index: number) => {
+    if (taskDoneIndexes.indexOf(index) == -1) {
+      setTaskDoneIndexes([...taskDoneIndexes, index]);
+    } else {
+      taskDoneIndexes.splice(taskDoneIndexes.indexOf(index), 1);
+      setTaskDoneIndexes([...taskDoneIndexes]);
+    }
   };
 
   return (
@@ -38,8 +48,19 @@ function TodoList() {
         <ul className="list-group">
           {list.map((it, index) => (
             <li className="list-group-item">
-              {it}
-              <Button text="Done" onButtonClick={addToList} />
+              <span
+                className={
+                  taskDoneIndexes.includes(index)
+                    ? "text-muted text-decoration-line-through"
+                    : ""
+                }
+              >
+                {it}
+              </span>
+              <Button
+                text={taskDoneIndexes.includes(index) ? "Reopen" : "Done"}
+                onButtonClick={() => taskDone(index)}
+              />
               <Button
                 text="Delete"
                 onButtonClick={() => deleteFromList(index)}
@@ -53,3 +74,8 @@ function TodoList() {
 }
 
 export default TodoList;
+
+// improvements
+// 1. Store object {id, text, done} rather than different states for text and taskdonelist
+// 2. Memoisation
+// 3. CSS
